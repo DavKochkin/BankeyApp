65e3aedfa25e0b69fcf778fdf21768494d118ded
David <kochkin.david@gmail.com>
2023-05-05 15:38:36 +0300

    Notification center

---
+++ Bankey/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bankey
+//
+//  Created by David Kochkin on 05.05.2023.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
